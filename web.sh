6cf38f9faab512167821b5cb8fdb4441fc57ee29
aphshir <45636296+aphshir@users.noreply.github.com>
2024-01-12 23:49:42 +0100

    Add files via upload

--- web.sh
+++ web.sh
@@ -1,12 +1,24 @@
 #!/bin/bash
-
-if [ $# -ne 1 ]; then
-	echo "[PAGE][fatal] $# arguments donnés attendu 1"
-	exit
-else if [ ! -d $1 ]; then
-	echo "[PAGE][fatal] $1 n'est pas un répertoire"
-	exit
-fi
-echo "<!DOCTYPE html><html><head></head><body><h1>REG OF</h1><p>This is a blank template for a web page.</p></body></html>" > test.html
-for years in $1 do
-	
+source ./config.cfg
+#if [ $# -ne 1 ]; then
+#	echo "[PAGE][fatal] $# arguments donnés attendu 1"
+#	exit
+#else if [ ! -d $1 ]; then
+##	echo "[PAGE][fatal] $1 n'est pas un répertoire"
+#	exit
+#fi
+echo -e "<!DOCTYPE html><html><head></head><body><h1>reg of <a href=\"$archivepath\">$archivepath</a></h1>" > list.html
+for years in "$archivepath"/*; do
+	echo -e "<h2><a href=\"$years\">$(basename $years)</a></h2>" >> list.html
+	echo "<ul>" >> list.html
+	for months in "$years"/*; do
+		echo -e "<li><a href=\"$months\">$(basename $months)</a></li>" >> list.html
+		echo "<ul>" >> list.html
+		for files in "$months"/*; do
+			echo "<li><a href=\"$files\">$(basename $files)</a></li>" >> list.html
+		done
+		echo "</ul>" >> list.html
+	done
+	echo "</ul>" >> list.html
+done
+exit
